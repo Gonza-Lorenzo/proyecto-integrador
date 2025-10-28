94f555015a05ff9e8aabfa5a1066188a170d585a
Gonzalo Lorenzo <gonzalorenzo55@gmail.com>
2025-10-28 14:42:55 -0300

    Subo proyecto integrador

---
+++ src/components/donde-vive-estado/Hijo2.jsx
@@ -0,0 +1,15 @@
+//rafce
+
+import React, { useState } from 'react'
+
+const Hijo2 = ({ mensaje }) => {
+
+  return (
+    <>
+        <h2 className="">Hijo2</h2>
+        <p>{mensaje}</p>
+    </>
+  )
+}
+
+export default Hijo2
